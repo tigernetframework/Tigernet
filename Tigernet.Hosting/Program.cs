cad4372fa2e452f554d69775a50cbe73029932f9
Mukhammadkarim Tukhtaboev <115141735+mtukhtaboev@users.noreply.github.com>
2023-02-04 04:16:38 +0500

    Add project files.

---
+++ Tigernet.Hosting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Tigernet.Hosting;
+
+TigernetHostBuilder host = new TigernetHostBuilder("http://localhost:5001/");
+
+// add route to the host
+host.MapRoute("/", async context =>
+{
+    var response = context.Response;
+    response.ContentType = "text/html";
+    var content = Encoding.UTF8.GetBytes("<h1>Welcome to Tigernet Framework</h1>");
+    response.ContentLength64 = content.Length;
+    using (var output = response.OutputStream)
+    {
+        await output.WriteAsync(content, 0, content.Length);
+    }
+});
+
+host.MapRoute("/about", async context =>
+{
+    var response = context.Response;
+    response.ContentType = "text/html";
+    var content = Encoding.UTF8.GetBytes("<h1>About Tigernet Framework</h1>");
+    response.ContentLength64 = content.Length;
+    using (var output = response.OutputStream)
+    {
+        await output.WriteAsync(content, 0, content.Length);
+    }
+});
+
+host.MapRoute("/contact", async context =>
+{
+    var response = context.Response;
+    response.ContentType = "text/html";
+    var content = Encoding.UTF8.GetBytes("<h1>Contact Tigernet Framework</h1>");
+    response.ContentLength64 = content.Length;
+    using (var output = response.OutputStream)
+    {
+        await output.WriteAsync(content, 0, content.Length);
+    }
+});
+
+await host.Start();
---
+++ Tigernet.Hosting/TigernetHostBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tigernet.Hosting
+{
+    public class TigernetHostBuilder
+    {
+        private readonly string _prefix;
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Dictionary<string, Func<HttpListenerContext, Task>> _routes = new Dictionary<string, Func<HttpListenerContext, Task>>();
+
+        public TigernetHostBuilder(string prefix)
+        {
+            _prefix = prefix;
+            _listener.Prefixes.Add(prefix);
+        }
+
+
+        public void MapRoute(string route, Func<HttpListenerContext, Task> handler)
+        {
+            _routes.Add(route, handler);
+        }
+
+        public async Task Start()
+        {
+            _listener.Start();
+            Console.WriteLine("Server is running on " + _prefix);
+            while (true)
+            {
+                var context = await _listener.GetContextAsync();
+                await HandleRequest(context);
+            }
+        }
+
+        private async Task HandleRequest(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            Func<HttpListenerContext, Task> handler;
+            if (_routes.TryGetValue(request.RawUrl, out handler))
+            {
+                await handler(context);
+            }
+            else
+            {
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                response.Close();
+            }
+        }
+    }
+}
