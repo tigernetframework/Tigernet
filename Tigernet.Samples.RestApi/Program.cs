82bb61113718e24650ee55f1fd56aa55b9661ad7
Mukhammadkarim Tukhtaboev <115141735+mtukhtaboev@users.noreply.github.com>
2023-03-27 06:16:49 +0500

    uncomment middlewares

--- Tigernet.Samples.RestApi/Program.cs
+++ Tigernet.Samples.RestApi/Program.cs
@@ -1,23 +1,23 @@
 ﻿using Tigernet.Hosting;
 using Tigernet.Samples.RestApi.Resters;
 
-var builder = new TigernetHostBuilder("http://localhost:5001/");
+var builder = new TigernetHostBuilder("http://localhost:5000/");
 
 // add rester to the host
 builder.MapRester<UsersRester>();
 builder.MapRester<HomeRester>();
 
-builder.UseAsync(async (context) =>
-{
-    Console.WriteLine("Middleware 1");
-    await Task.CompletedTask;
-});
+//builder.UseAsync(async (context) =>
+//{
+//    Console.WriteLine("Middleware 1");
+//    await Task.CompletedTask;
+//});
 
-builder.UseAsync(async (context) =>
-{
-    Console.WriteLine("Middleware 2");
-    await Task.CompletedTask;
-});
+//builder.UseAsync(async (context) =>
+//{
+//    Console.WriteLine("Middleware 2");
+//    await Task.CompletedTask;
+//});
 
 await builder.Start();
 
