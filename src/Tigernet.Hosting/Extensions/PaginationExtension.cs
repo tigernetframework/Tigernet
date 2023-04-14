e936f7f391ccb30185942cb3969c4fcb57e3bcf4
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-04-14 21:28:01 +0500

    Native pagination features for collections

--- src/Tigernet.Hosting/Extensions/PaginationExtension.cs
+++ src/Tigernet.Hosting/Extensions/PaginationExtension.cs
@@ -1,4 +1,5 @@
-﻿using Tigernet.Hosting.Models.Query;
+﻿using System.IO;
+using Tigernet.Hosting.Models.Query;
 
 namespace Tigernet.Hosting.Extensions
 {
@@ -13,14 +14,13 @@ namespace Tigernet.Hosting.Extensions
         /// <returns>An IEnumerable<T> object that contains the items for the current page.</returns>
         public static IEnumerable<T> Paginate<T>(this IEnumerable<T> source, PaginationOptions options)
         {
+            if (source == null || options == null)
+                throw new ArgumentException("Can't apply filter to null source or with null filter options");
+
             var itemsForSkip = (options.PageToken - 1) * options.PageSize;
             var totalCount = source.Count();
-
             if (itemsForSkip >= totalCount && totalCount > 0)
-            {
                 itemsForSkip = totalCount - totalCount % options.PageSize;
-            }
-
             return source.Skip(itemsForSkip).Take(options.PageSize);
         }
         /// <summary>
@@ -32,14 +32,13 @@ namespace Tigernet.Hosting.Extensions
         /// <returns>An IQueryable<T> object that contains the items for the current page.</returns>
         public static IQueryable<T> Paginate<T>(this IQueryable<T> source, PaginationOptions options)
         {
+            if (source == null || options == null)
+                throw new ArgumentException("Can't apply filter to null source or with null filter options");
+            
             var itemsForSkip = (options.PageToken - 1) * options.PageSize;
             var totalCount = source.Count();
-
             if (itemsForSkip >= totalCount && totalCount > 0)
-            {
                 itemsForSkip = totalCount - totalCount % options.PageSize;
-            }
-
             return source.Skip(itemsForSkip).Take(options.PageSize);
         }
 
