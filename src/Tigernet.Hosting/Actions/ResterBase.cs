26679f1931a839b7b127a722abebe1581414b7db
Faxriddin Xushnazarov <xushnazarov1555@outlook.com>
2023-05-12 03:00:29 +0500

    Property naming policy changed to camel case

--- src/Tigernet.Hosting/Actions/ResterBase.cs
+++ src/Tigernet.Hosting/Actions/ResterBase.cs
@@ -4,9 +4,13 @@ namespace Tigernet.Hosting.Actions
 {
     public abstract class ResterBase
     {
-        public object Ok(object data)
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions()
         {
-            return JsonSerializer.Serialize(data);
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        public object Ok<T>(T data)
+        {
+            return JsonSerializer.Serialize(data, _jsonSerializerOptions);
         }
     }
 }
